8576967fe8db967a0959adc4de32a710bd065c9b
Julian F. <julianf@volan.top>
2026-06-01 15:54:35 -0400

    support single instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -18,6 +18,13 @@ pub fn run() {
             Some(vec!["--minimized"]),
         ))
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.show();
+                let _ = window.unminimize();
+                let _ = window.set_focus();
+            }
+        }))
         .invoke_handler(tauri::generate_handler![
             engine::toggle_pause,
             engine::get_pause_state,
